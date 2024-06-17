46585c0c6b95482a35486d597833467b60697b01
NadirMansur <nadirmansur89@gmail.com>
2024-06-17 01:45:45 -0300

    se agrega controller deletePhotoById

---
+++ src/controllers/galeria/deletePhotoById.js
@@ -0,0 +1,20 @@
+const { Galeria } = require("../../db");
+
+const deletePhoto = async (req, res, next) => {
+  try {
+    const photo = await Galeria.findByPk(req.params.id);
+    if (photo) {
+      await photo.destroy();
+      res.status(204).json({
+        message: "Foto eliminada satisfactoriamente",
+        success: true,
+      });
+    } else {
+      res.status(404).json({ message: "No se encontro el la Imagen" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = deletePhoto;
